38a325021bf72df67304b1d98ee8bbdac5ea0ebb
Regina <qregi.1@outlook.com>
2020-06-21 23:37:59 +0100

    editing homepage css and adding subtitle field

--- home/models.py
+++ home/models.py
@@ -1,10 +1,13 @@
+from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
     
     
 class HomePage(Page):
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
         FieldPanel('body', classname="full"),
     ]
