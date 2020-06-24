fd491030519640d75523124a632787e2caeb4eca
Regina <qregi.1@outlook.com>
2020-06-24 09:48:47 +0100

    changing map model and template

--- map/models.py
+++ map/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from wagtail.core.models import Page
-from wagtailgmaps.edit_handlers import MapFieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 # Create your models here.
 class MapPage(Page):
-    # Wagtailgmaps expects a `CharField` (or any other field that renders as a text input)
-    formatted_address = models.CharField(max_length=255)
-    latlng_address = models.CharField(max_length=255)
+    body = RichTextField(blank=True)
 
-    # Use the `MapFieldPanel` just like you would use a `FieldPanel`
     content_panels = Page.content_panels + [
-        MapFieldPanel('formatted_address'),
-        MapFieldPanel('latlng_address', latlng=True),
+        FieldPanel('body'),
     ]
