bdf14c21ef7f60d06512039b178863834b3908ea
Regina <qregi.1@outlook.com>
2020-06-25 01:37:34 +0100

    homepage migration and editing css very slightly for homepage. Mainly just colour change from white to light grey.

--- home/models.py
+++ home/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
     
@@ -14,10 +13,8 @@ class HomePage(Page):
         on_delete=models.SET_NULL
     )
     subtitle = models.CharField(max_length=100, blank=True, null=True)
-    body = RichTextField(blank=True)
     
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         ImageChooserPanel('background_image'),
-        FieldPanel('body', classname="full"),
     ]
