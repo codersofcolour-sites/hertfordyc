7f9ad5fa376b5d6368aee40914b8c16deb86f810
Regina <qregi.1@outlook.com>
2020-06-25 01:17:40 +0100

    a crude attempt to make homepage css more presentable and responsive.

--- home/models.py
+++ home/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
     
     
 class HomePage(Page):
+    background_image =  models.ForeignKey(
+        'wagtailimages.Image',
+        related_name="background_image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
     subtitle = models.CharField(max_length=100, blank=True, null=True)
     body = RichTextField(blank=True)
+    
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
+        ImageChooserPanel('background_image'),
         FieldPanel('body', classname="full"),
     ]
--- mysite/static/js/mysite.js
+++ mysite/static/js/mysite.js
@@ -1,2 +1,9 @@
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAH7BwUI92aJzMYB3D57Vvm_mqwVEGyBcgY&callback=initMap"
-  type="text/javascript"></script>
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
