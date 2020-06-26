52abb618a6f409cbbe38de567a7fd4e333b2fcd4
Regina <qregi.1@outlook.com>
2020-06-26 09:34:28 +0100

    renaming image field to background image in blog page

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
@@ -16,14 +17,27 @@ class BlogIndexPage(Page):
         """Adding custom stuff to our context"""
         context = super().get_context(request, *args, **kwargs)
         live_blogpages = self.get_children().live()
-        context['blogpages'] = BlogPage.objects.live().public()
+        all_posts = BlogPage.objects.live().public().order_by('-first_published_at')
+
+        paginator = Paginator(all_posts, 1)
+
+        page = request.GET.get("page")
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+
+        context["blogpages"] = posts
+
         return context
     
 
 class BlogPage(Page):
     author = models.CharField(max_length=100, default="")
     date = models.DateField("Post date")
-    image = models.ForeignKey(
+    background_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         on_delete=models.SET_NULL
@@ -35,7 +49,7 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('author'),
         FieldPanel('date'),
-        ImageChooserPanel('image'),
+        ImageChooserPanel('background_image'),
         FieldPanel('subtitle'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
