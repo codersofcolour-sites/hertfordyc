5cfababe5c5ac9d33da6cfd0b21a59a2b8e32e68
Regina <qregi.1@outlook.com>
2020-06-28 17:02:13 +0100

    adding external url title to flex cards

--- streams/blocks.py
+++ streams/blocks.py
@@ -10,7 +10,8 @@ class CardBlock(blocks.StructBlock):
                ('title', blocks.CharBlock(required=True, max_length=40)),
                ('text', blocks.TextBlock(required=True, max_length=200)),
                ('button_page', blocks.PageChooserBlock(required=False)),
-               ('button_url', blocks.URLBlock(required=False, help_text="")),
+               ('button_url', blocks.URLBlock(required=False, help_text="use if you want to go to external links")),
+               ('button_url_title', blocks.CharBlock(required=False, max_length=40, help_text="if you have an external link, give the button for it a title.")),
             ]
         )
     )
