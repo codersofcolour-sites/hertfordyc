90b1880a9315b70479108be1d3ede3468e8de6ce
Regina <qregi.1@outlook.com>
2020-07-19 15:31:32 +0100

    adding ImageChooserBlock

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core import blocks
 from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class BlogIndexPage(Page):
@@ -55,6 +56,7 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = StreamField([
         ('heading', blocks.CharBlock(icon="title", null=True, blank=True)),
+        ('image', ImageChooserBlock(required=False)),
         ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['h3', 'h4', 'centre', 'bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link', 'embed'], null=True, blank=True)),
         ('embed', EmbedBlock(icon="media", null=True, blank=True)),
         ('embed_HTML', blocks.RawHTMLBlock(required=False, help_text="use this to embed elements that do not embed using the normal embed block, e.g. google forms", null=True, blank=True)),
--- flex/models.py
+++ flex/models.py
@@ -6,6 +6,7 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.embeds.blocks import EmbedBlock
 from streams.blocks import CardBlock
+from wagtail.images.blocks import ImageChooserBlock
 
 
 # Create your models here.
@@ -14,6 +15,7 @@ class FlexPage(Page):
 
     body = StreamField([
         ('heading', blocks.CharBlock(icon="title", null=True, blank=True)),
+        ('image', ImageChooserBlock(required=False)),
         ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['h3', 'h4', 'centre', 'bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link', 'embed'],  null=True, blank=True)),
         ('embed', EmbedBlock(icon="media", null=True, blank=True)),
         ('embed_HTML', blocks.RawHTMLBlock(required=False, help_text="use this to embed elements that do not embed using the normal embed block, e.g. google forms", null=True, blank=True)),
