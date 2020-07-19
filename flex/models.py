32171f75f1e83d64f95e642b6f90ce9a3c00a297
Regina <qregi.1@outlook.com>
2020-07-19 15:09:00 +0100

    adding centre text option to flex pages

--- flex/models.py
+++ flex/models.py
@@ -14,7 +14,7 @@ class FlexPage(Page):
 
     body = StreamField([
         ('heading', blocks.CharBlock(icon="title", null=True, blank=True)),
-        ('paragraph', blocks.RichTextBlock(icon="pilcrow", null=True, blank=True)),
+        ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['h3', 'h4', 'centre', 'bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link', 'embed'],  null=True, blank=True)),
         ('embed', EmbedBlock(icon="media", null=True, blank=True)),
         ('embed_HTML', blocks.RawHTMLBlock(required=False, help_text="use this to embed elements that do not embed using the normal embed block, e.g. google forms", null=True, blank=True)),
         ('cards', CardBlock()),
